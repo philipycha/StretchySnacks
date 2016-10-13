58c0fe12e515ffb3ad5cd4bd7b507125f3470f54
philipycha <philip.yc.ha@gmail.com>
2016-10-13 15:10:38 -0700

    add button animates and changes height of view

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK : PROPERTIES
+    
+    @IBOutlet weak var navigationBarHeight: NSLayoutConstraint!
+    @IBOutlet weak var button: UIButton!
+    
+    var animator:UIDynamicAnimator!
 
+    
+    //MARK: FUNCTIONS
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
     }
 
-
+    @IBAction func plusIconPressed(_ sender: UIButton) {
+        
+        animateBarHeight()
+        rotateButton()
+        
+    }
+    
+    func animateBarHeight() {
+        if navigationBarHeight.constant == 64 {
+            
+            navigationBarHeight.constant = 200
+            
+        } else if navigationBarHeight.constant == 200 {
+            
+            navigationBarHeight.constant = 64
+            
+        }
+        
+        UIView.animate(withDuration: 0.6,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.4,
+                       initialSpringVelocity: 0.0,
+                       options: [],
+                       animations: {
+                        
+            self.view.layoutIfNeeded()
+                        
+            }, completion: nil)
+    }
+    
+    func rotateButton() {
+        
+        UIView.animate(withDuration: 0.6,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.4,
+                       initialSpringVelocity: 0.0,
+                       options: [],
+                       animations: {
+                        
+            self.button.transform = self.button.transform.rotated(by: CGFloat(M_PI_4))
+                        
+            }, completion: nil)
+        
+    }
 }
 
